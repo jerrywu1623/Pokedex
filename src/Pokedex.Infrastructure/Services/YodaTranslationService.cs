c35a8188be0cc0c6ac0b01e0955252e3bd0e3759
JerryWu <jerry.wu@codeworldwide.com>
2021-08-19 23:17:42 +0100

    implemented yoda, shakespeare translation service

---
+++ src/Pokedex.Infrastructure/Services/YodaTranslationService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Pokedex.Application;
+using Pokedex.Application.Common.Interfaces;
+using Pokedex.Domain.Entities;
+using Pokedex.Domain.Enums;
+
+namespace Pokedex.Infrastructure.Services
+{
+    public class YodaTranslationService: ITranslationService
+    {
+        private IHttpClientFactory _httpClientFactory;
+        private ILogger<YodaTranslationService> _logger;
+        private const string YODA_TRANSLATE = "yoda";
+
+        public YodaTranslationService(IHttpClientFactory httpClientFactory, ILogger<YodaTranslationService> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        public TranslationOptions TransactionOption => TranslationOptions.Yoda;
+
+        public async Task<string> TranslateAsync(string text, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                _logger.LogInformation("start calling YodaTranslationService");
+                var client = _httpClientFactory.CreateClient(Constants.TRANSLATION_API_CLIENT_NAME);
+                var requestBody = new { text = text };
+                var response = await client.PostAsJsonAsync(YODA_TRANSLATE, requestBody, cancellationToken);
+
+                response.EnsureSuccessStatusCode();
+                var funTranslation = await response.Content.ReadFromJsonAsync<FunTranslation>(cancellationToken: cancellationToken);
+                
+                _logger.LogInformation($"end calling YodaTranslationService, response: {JsonSerializer.Serialize(funTranslation)}");
+                return funTranslation.contents.translated;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "YodaTranslationService error");
+                return text;
+            }
+        }
+    }
+}
