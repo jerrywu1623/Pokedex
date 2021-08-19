086453074c5c47c3fecdc17b25bafb92c94224e3
jerrywu <jerry.wu@codeworldwide.com>
2021-08-19 21:33:56 +0100

    created new endpoint for getting pokemon spec

---
+++ src/Pokedex.Web/Controllers/PokemonsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Application.PokemonSpecs.Queries;
+using Pokedex.Application.ViewModels;
+
+namespace Pokedex.Web.Controllers
+{
+    public class PokemonsController : ApiControllerBase
+    {
+        [HttpGet("{name}")]
+        public async Task<ActionResult<PokemonSpecVm>> Get(string name, CancellationToken cancellationToken)
+        {
+            return await Mediator.Send(new GetPokemonSpecQuery() { name = name }, cancellationToken);
+        }
+    }
+}
