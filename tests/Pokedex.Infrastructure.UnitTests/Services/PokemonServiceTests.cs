5c7db515a4cab42d487e4a0e2a001e15c9780cf5
jerrywu <jerry.wu@codeworldwide.com>
2021-08-20 00:22:19 +0100

    added unit tests for PokemonService

---
+++ tests/Pokedex.Infrastructure.UnitTests/Services/PokemonServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Pokedex.Infrastructure.Services;
+using NSubstitute;
+using Pokedex.Domain.Entities;
+
+namespace Pokedex.Infrastructure.UnitTests.Services
+{
+    [TestFixture]
+    public class PokemonServiceTests
+    {
+        private ILogger<PokemonService> _logger;
+        private IHttpClientFactory _httpClientFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = Substitute.For<ILogger<PokemonService>>();
+            _httpClientFactory = Substitute.For<IHttpClientFactory>();
+        }
+
+        [Test]
+        public async Task CallPokemonApiSuccess()
+        {
+            //arrange
+            var name = "test";
+            var pokemonSpec = new PokemonSpec
+            {
+                name = "test",
+                habitat = new Habitat
+                {
+                    name = "rare"
+                },
+                is_legendary = true,
+                flavor_text_entries = new []{new Flavor_Text_Entry{flavor_text = "this is test"}}
+            };
+            var client = new HttpClient(new FakePokemonHttpMessageHandler(pokemonSpec));
+            client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+            _httpClientFactory.CreateClient(Arg.Any<string>()).Returns(client);
+            var pokemonService = new PokemonService(_logger, _httpClientFactory);
+
+            //act
+            var result = await pokemonService.GetPokemonSpec(name);
+            
+            //assert
+            Assert.AreEqual(pokemonSpec.name, result.name);
+            Assert.AreEqual(pokemonSpec.habitat.name, result.habitat.name);
+            Assert.IsTrue(result.is_legendary);
+        }
+    }
+}
